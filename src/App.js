fe3cef8830e43dbe99fd0f122d10d84ea78c29d1
Dave Harms <daveharmswebdev@gmail.com>
2016-12-31 10:04:09 -0600

    more setup, added barebones reducer and eslintrc

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+       apiKey: 'AIzaSyAkCm3T1X2lbHmOrF3dkE6WdZk-PapBuhg',
+       authDomain: 'manager-965c9.firebaseapp.com',
+       databaseURL: 'https://manager-965c9.firebaseio.com',
+       storageBucket: 'manager-965c9.appspot.com',
+       messagingSenderId: '504395830386'
+     };
+
+     firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
